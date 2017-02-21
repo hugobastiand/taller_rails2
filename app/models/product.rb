49b4cb9713926e9822cc4527f8146fc8e137b038
Hugo Duran <hu.durannn@gmail.com>
2017-02-21 17:40:05 -0300

    add scope_premium and scope last_n

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,15 @@
 class Product < ApplicationRecord
-  belongs_to :category
-  validates :price, presence: true
-  before_validation :set_premium
-  def set_premium
-  	self.premium ||= false
-  end	
+	  belongs_to :category
+	  
+	  scope :scope_premium, ->{ where(premium: true)}
+	  scope :last_n, ->(cant) {order('id DESC').limit(cant)}
+
+	  validates :price, presence: true
+	  before_validation :set_premium
+	  
+
+	  def set_premium
+	  	self.premium ||= false
+	  end	
+
 end
