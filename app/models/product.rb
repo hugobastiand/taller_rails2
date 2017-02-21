8b246c4179fe450df1f33d76765cefc81072184c
Hugo Duran <hu.durannn@gmail.com>
2017-02-21 16:30:59 -0300

    add key foream and column premium and price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :price, presence: true
+  before_validation :set_premium
+  def set_premium
+  	self.premium ||= false
+  end	
 end
